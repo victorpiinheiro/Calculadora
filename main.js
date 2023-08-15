1ee5af90f9220322d7e5f6d96fcd2db633cd54ce
Victor Pinheiro <contato.vspinheiro@gmail.com>
2023-08-15 11:55:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 
-function Calculadora () {
+function Calculadora() {
     this.digito = document.querySelector('.digitos');
     this.display = document.querySelector('.display');
 
     this.inicia = () => {
         this.capturaCliques();
+        this.capturaEnter();
+    }
+
+    this.capturaEnter = () => {
+        document.addEventListener('keypress', (e) => {
+            if (e.keyCode !== 13) return;
+            this.realizaConta();
+        });
     }
 
     this.capturaCliques = () => {
@@ -14,12 +22,13 @@ function Calculadora () {
             if (el.classList.contains('bt-eq')) this.realizaConta();
             if (el.classList.contains('bt-apaga')) this.Apaga();
             if (el.classList.contains('bt-del')) this.apagaUtimoDigito();
-        })
+        });
     }
 
     this.numerosDigitados = (e) => {
-       this.digito.innerHTML += e.innerHTML;
-       this.display.value += e.innerHTML;
+        this.digito.innerHTML += e.innerHTML;
+        this.display.value += e.innerHTML;
+        this.display.focus();
     }
 
     this.Apaga = () => {
@@ -28,32 +37,27 @@ function Calculadora () {
     }
 
     this.apagaUtimoDigito = () => {
-        
         this.display.value = this.display.value.slice(0, -1);
         this.digito.innerHTML = this.digito.innerHTML.slice(0, -1);
-
-        if (!this.display.value ) {
+        if (!this.display.value) {
             this.digito.innerHTML = '';
-        }
-        
+        };
         this.display.focus();
-        
+
     }
 
     this.realizaConta = () => {
 
         try {
-            let conta = eval (this.display.value);
+            let conta = eval(this.display.value);
             if (!conta && conta !== 0) {
                 alert('Conta invalida');
             }
             this.display.value = conta;
         } catch (e) {
-            alert('Conta invalida');
-            
-        }
-        
-    }
+            alert('Conta inválida');
+        };
+    };
 }
 
 
